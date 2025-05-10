de15d65525a927300ec6f37fe38ba0d620602274
Ibrahim Hudson <you@example.com>
2025-05-10 16:24:36 -0400

    style changes

--- frontend/src/components/info.jsx
+++ frontend/src/components/info.jsx
@@ -2,10 +2,17 @@ export const Intro = () => {
   return (
     <>
       <p>
-        Welcome to "I don't want to write 100 emails" aka. ID:10T! An email
-        template website created to make your emailing process easier. Enter in
-        an email template, give it a name and inputs then you can use it
-        whenever you need, Have fun!
+        Welcome to "I don't want to write 10 emails" aka. ID:10T! This
+        innovative, full-stack email template generator, powered by the Gemini
+        API, to make your email outreach and communication more efficient. The
+        goal of this project is to help you save time and effort by providing
+        you with a template for your emails, so you can focus on what really
+        matters - getting your message across.
+        <p style={{ color: "orange" }}>
+          Future features include: Customizable templates, Integration with your
+          email client, Analytics to track your email performance, And much
+          more!
+        </p>
       </p>
     </>
   );
